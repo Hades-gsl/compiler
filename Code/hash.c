2657b4153ab38b828a67fcef5c4ceb97f18473cc
hades <gushilei666@qq.com>
2024-03-29 11:50:36 +0800

    feat: add privdata and ht_type field

--- Code/hash.c
+++ Code/hash.c
@@ -1,7 +1,14 @@
 #include "hash.h"
 
+/* -------------------------- private prototypes ---------------------------- */
+
 static unsigned long _ht_next_power(unsigned long size);
 static unsigned long _ht_get_index(hash_table *ht, const void *key);
+static void _ht_reset(hash_table *ht);
+static int _ht_init(hash_table *ht, ht_type *type, void *privdata_ptr);
+static int _ht_clear(hash_table *ht);
+
+/* -------------------------- hash functions -------------------------------- */
 
 /* Generic hash function (a popular one from Bernstein) */
 static unsigned int ht_gen_hash_function(const char *str) {
@@ -11,28 +18,24 @@ static unsigned int ht_gen_hash_function(const char *str) {
   return hash;
 }
 
+/* ----------------------------- API implementation ------------------------- */
+
 /* Create a new hash table */
-static hash_table *ht_create() {
+static hash_table *ht_create(ht_type *type, void *privdata_ptr) {
   hash_table *ht = malloc(sizeof(hash_table));
   if (!ht) return NULL;
 
-  ht->size = HT_INITIAL_SIZE;
-  ht->used = 0;
-  ht->mask = ht->size - 1;
-  ht->table = calloc(ht->size, sizeof(hash_entry *));
-  if (!ht->table) {
-    free(ht);
-    return NULL;
-  }
+  _ht_init(ht, type, privdata_ptr);
 
   return ht;
 }
 
 /* Expand the hash table */
 static int ht_expand(hash_table *ht, unsigned long size) {
-  hash_table *new_ht = ht_create();
+  hash_table *new_ht = malloc(sizeof(hash_table)); /* new hash table */
   if (!new_ht) return HT_ERR;
 
+  _ht_init(new_ht, ht->type, ht->privdata);
   new_ht->size = _ht_next_power(size);
   new_ht->mask = new_ht->size - 1;
   new_ht->used = ht->used;
@@ -42,17 +45,26 @@ static int ht_expand(hash_table *ht, unsigned long size) {
     return HT_ERR;
   }
 
+  /* Copy all the elements from the old to the new table:
+   * note that if the old hash table is empty ht->size is zero,
+   * so ht_xpand just creates an hash table. */
   for (unsigned long i = 0; i < ht->size; i++) {
     hash_entry *entry = ht->table[i];
     while (entry) {
-      ht_add(new_ht, entry->key, entry->val);
-      entry = entry->next;
+      hash_entry *next = entry->next;
+
+      unsigned long index = htHashKey(ht, entry->key) & new_ht->mask;
+      entry->next = new_ht->table[index];
+      new_ht->table[index] = entry;
       ht->used--;
+
+      entry = next;
     }
   }
 
   assert(ht->used == 0);
   free(ht->table);
+  free(ht);
 
   ht = new_ht;
 
@@ -61,16 +73,20 @@ static int ht_expand(hash_table *ht, unsigned long size) {
 
 /* Add a new key-value pair to the hash table */
 static int ht_add(hash_table *ht, void *key, void *val) {
+  /* Get the index of the new element, or -1 if
+   * the element already exists. */
   int index = _ht_get_index(ht, key);
   if (index == -1) return HT_ERR;
 
+  /* Allocates the memory and stores key */
   hash_entry *entry = malloc(sizeof(hash_entry));
   if (!entry) return HT_ERR;
 
-  entry->key = key;
-  entry->val = val;
   entry->next = ht->table[index];
   ht->table[index] = entry;
+
+  htSetHashKey(ht, entry, key);
+  htSetHashVal(ht, entry, val);
   ht->used++;
 
   return HT_OK;
@@ -81,12 +97,23 @@ static int ht_add(hash_table *ht, void *key, void *val) {
  * element with such key and ht_replace() just performed a value update
  * operation. */
 static int ht_replace(hash_table *ht, void *key, void *val) {
+  /* Try to add the element. If the key
+   * does not exists ht_add will succeed. */
   if (ht_add(ht, key, val) == HT_OK) return 1;
 
+  /* It already exists, get the entry */
   hash_entry *entry = ht_find(ht, key);
   if (!entry) return 0;
 
-  entry->val = val;
+  /* Free the old value and set the new one */
+  /* Set the new value and free the old one. Note that it is important
+   * to do that in this order, as the value may just be exactly the same
+   * as the previous one. In this context, think to reference counting,
+   * you want to increment (set), and then decrement (free), and not the
+   * reverse. */
+  void *old_val = entry->val;
+  htSetHashVal(ht, entry, val);
+  htFreeEntryVal(ht, entry);
 
   return 0;
 }
@@ -95,17 +122,20 @@ static int ht_replace(hash_table *ht, void *key, void *val) {
 static int ht_delete(hash_table *ht, const void *key) {
   if (ht->used == 0) return HT_ERR;
 
-  unsigned long index = GET_HASH_INDEX(ht, key);
+  unsigned long index = htHashKey(ht, key) & ht->mask;
 
   hash_entry *entry = ht->table[index];
   hash_entry *prev = NULL;
   while (entry) {
-    if (!strcmp(entry->key, key)) {
+    if (htCompareHashKeys(ht, entry->key, key)) {
       if (prev) {
         prev->next = entry->next;
       } else {
         ht->table[index] = entry->next;
       }
+
+      htFreeEntryKey(ht, entry);
+      htFreeEntryVal(ht, entry);
       free(entry);
       ht->used--;
       return HT_OK;
@@ -119,30 +149,26 @@ static int ht_delete(hash_table *ht, const void *key) {
 
 /* Clear & Release the hash table */
 static void ht_release(hash_table *ht) {
-  for (unsigned long i = 0; i < ht->size; i++) {
-    hash_entry *entry = ht->table[i];
-    while (entry) {
-      hash_entry *next = entry->next;
-      free(entry);
-      entry = next;
-    }
-  }
-  free(ht->table);
+  _ht_clear(ht);
   free(ht);
 }
 
 /* Find an element in the hash table */
 static hash_entry *ht_find(hash_table *ht, const void *key) {
-  unsigned long index = GET_HASH_INDEX(ht, key);
+  if (ht->used == 0) return NULL;
+
+  unsigned long index = htHashKey(ht, key) & ht->mask;
 
   hash_entry *entry = ht->table[index];
   while (entry) {
-    if (!strcmp(entry->key, key)) return entry;
+    if (htCompareHashKeys(ht, key, entry->key)) return entry;
     entry = entry->next;
   }
 
   return NULL;
 }
+
+/* ------------------------- private functions ------------------------------ */
 
 /* Our hash table capability is a power of two */
 static unsigned long _ht_next_power(unsigned long size) {
@@ -163,13 +189,52 @@ static unsigned long _ht_get_index(hash_table *ht, const void *key) {
     if (!ht_expand(ht, ht->size * 2)) return -1;
   }
 
-  unsigned long index = GET_HASH_INDEX(ht, key);
+  unsigned long index = htHashKey(ht, key) & ht->mask;
 
   hash_entry *entry = ht->table[index];
   while (entry) {
-    if (!strcmp(entry->key, key)) return -1;
+    if (htCompareHashKeys(ht, key, entry->key)) return -1;
     entry = entry->next;
   }
 
   return index;
+}
+
+/* Reset an hashtable already initialized with ht_init(). */
+static void _ht_reset(hash_table *ht) {
+  ht->table = NULL;
+  ht->size = 0;
+  ht->mask = 0;
+  ht->used = 0;
+}
+
+/* Initialize the hash table */
+static int _ht_init(hash_table *ht, ht_type *type, void *privdata_ptr) {
+  _ht_reset(ht);
+  ht->type = type;
+  ht->privdata = privdata_ptr;
+  return HT_OK;
+}
+
+/* Destroy an entire hash table */
+static int _ht_clear(hash_table *ht) {
+  /* Free all the elements */
+  for (unsigned long i = 0; i < ht->size; i++) {
+    hash_entry *entry = ht->table[i];
+    while (entry) {
+      hash_entry *next = entry->next;
+      htFreeEntryKey(ht, entry);
+      htFreeEntryVal(ht, entry);
+      free(entry);
+      ht->used--;
+      entry = next;
+    }
+  }
+
+  /* Free the table and the allocated cache structure */
+  free(ht->table);
+  /* Re-initialize the table */
+  _ht_reset(ht);
+
+  return HT_OK; /* never fails */
 }
--- Code/hash.h
+++ Code/hash.h
@@ -9,21 +9,73 @@ typedef struct hash_entry {
   struct hash_entry *next;
 } hash_entry;
 
+typedef struct ht_type {
+  unsigned int (*hash_function)(const void *key);
+  void *(*key_dup)(void *privdata, const void *key);
+  void *(*val_dup)(void *privdata, const void *obj);
+  int (*key_compare)(void *privdata, const void *key1, const void *key2);
+  void (*key_destructor)(void *privdata, void *key);
+  void (*val_destructor)(void *privdata, void *obj);
+} ht_type;
+
 typedef struct hash_table {
+  ht_type *type;
   unsigned size;
   unsigned mask;
   unsigned used;
   hash_entry **table;
+  void *privdata;
 } hash_table;
 
+/* This is the initial size of every hash table */
 #define HT_INITIAL_SIZE 4
+/* This is the maximum size of the hash table */
 #define LONG_MAX 2147483647L
-#define GET_HASH_INDEX(ht, key) (ht_gen_hash_function(key) & (ht)->mask)
+
+/* status code */
 #define HT_ERR -1
 #define HT_OK 0
 
+/* ------------------------------- Macros ------------------------------------*/
+#define htFreeEntryVal(ht, entry) \
+  if ((ht)->type->val_destructor) \
+  (ht)->type->val_destructor((ht)->privdata, (entry)->val)
+
+#define htSetHashVal(ht, entry, _val_)                         \
+  do {                                                         \
+    if ((ht)->type->val_dup)                                   \
+      entry->val = (ht)->type->val_dup((ht)->privdata, _val_); \
+    else                                                       \
+      entry->val = (_val_);                                    \
+  } while (0)
+
+#define htFreeEntryKey(ht, entry) \
+  if ((ht)->type->key_destructor) \
+  (ht)->type->key_destructor((ht)->privdata, (entry)->key)
+
+#define htSetHashKey(ht, entry, _key_)                         \
+  do {                                                         \
+    if ((ht)->type->key_dup)                                   \
+      entry->key = (ht)->type->key_dup((ht)->privdata, _key_); \
+    else                                                       \
+      entry->key = (_key_);                                    \
+  } while (0)
+
+#define htCompareHashKeys(ht, key1, key2)                    \
+  (((ht)->type->key_compare)                                 \
+       ? (ht)->type->key_compare((ht)->privdata, key1, key2) \
+       : (key1) == (key2))
+
+#define htHashKey(ht, key) (ht)->type->hash_function(key)
+
+#define htGetEntryKey(he) ((he)->key)
+#define htGetEntryVal(he) ((he)->val)
+#define htSlots(ht) ((ht)->size)
+#define htSize(ht) ((ht)->used)
+
+/* API */
 static unsigned int ht_gen_hash_function(const char *str);
-static hash_table *ht_create();
+static hash_table *ht_create(ht_type *type, void *privdata_ptr);
 static int ht_expand(hash_table *ht, unsigned long size);
 static int ht_add(hash_table *ht, void *key, void *val);
 static int ht_replace(hash_table *ht, void *key, void *val);
