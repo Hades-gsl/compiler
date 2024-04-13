fe8b5bf8468eb3f7f4df1d8007ba29556f8d3231
hades <gushilei666@qq.com>
2024-04-13 14:30:57 +0800

    feat: add semaintic analysis

--- Code/main.c
+++ Code/main.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "hash.h"
 #include "mbtree.h"
@@ -8,30 +9,32 @@
 #define FUNC_PTR_CAST(f) ((unsigned int (*)(const void*))f)
 
 MBTreeNode* root = NULL;
-MBTreeNode* empty = NULL;
 int has_error = 0;
 HashTable* ht = NULL;
 
 extern int yyparse();
 extern int yyrestart(FILE*);
+extern void semanticAnalysis(MBTreeNode* node);
+extern char* strdup(const char*);
 extern int yydebug;
 
 int keyCompare(void* privDataPtr, const void* a, const void* b) {
   return !strcmp(a, b);
 }
-
 void keyDestructor(void* privDataPtr, void* key) { free(key); }
+void valDestructor(void* privDataPtr, void* val) { freeType(val); }
+void* keyDup(void* privDataPtr, const void* key) { return strdup(key); }
+void* valDup(void* privDataPtr, const void* val) {
+  if (val == NULL) return NULL;
+  Type* v = (Type*)val;
+  return copyType[v->kind](v);
+}
 
-void valDestructor(void* privDataPtr, void* val) { freeFieldList(val); }
-
-void init() {
-  empty = newMBTreeNode(VAL_EMPTY, _Empty, -1);
-  assert(empty);
-
+static void init() {
   HtType* type = malloc(sizeof(HtType));
   *type = (HtType){.hashFunction = FUNC_PTR_CAST(htGenHashFunction),
-                   .keyDup = NULL,
-                   .valDup = NULL,
+                   .keyDup = keyDup,
+                   .valDup = valDup,
                    .keyCompare = keyCompare,
                    .keyDestructor = keyDestructor,
                    .valDestructor = valDestructor};
@@ -42,8 +45,16 @@ void init() {
   // yydebug = 1;
 }
 
+static void cleanup() {
+  freeMBTreeNode(root);
+  htRelease(ht);
+}
+
 int main(int argc, char** argv) {
   if (argc <= 1) return 1;
+
+  init();
+
   FILE* f = fopen(argv[1], "r");
   if (!f) {
     perror(argv[1]);
@@ -55,7 +66,12 @@ int main(int argc, char** argv) {
 
   fclose(f);
 
-  if (!has_error) displayMBTreeNode(root, 0);
+  if (!has_error) {
+    // displayMBTreeNode(root, 0);
+    semanticAnalysis(root);
+  }
+
+  cleanup();
 
   return 0;
 }
